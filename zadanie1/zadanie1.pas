2040d35bb2541a3e4e4077676740a057a7224ae0
Dawid Ciuba <dawidciuba.com@gmail.com>
2026-03-11 13:05:13 +0100

    3.5

--- zadanie1/zadanie1.pas
+++ zadanie1/zadanie1.pas
@@ -18,12 +18,41 @@ procedure GenerateNumbers();
         list[i] := Random(101);
     end;
 
-var
-  i: integer;
-begin
-  GenerateNumbers();
-  writeln('Lista: ');
-  for i := 1 to 50 do
-    write(list[i], ' ');
-  writeln;
+procedure BubbleSort(); 
+var 
+    i, j, n, temp: integer; 
+    flag: Boolean;
+begin 
+    n := 50; 
+    for i := 1 to n - 1 do 
+    begin 
+        flag := False; 
+        for j := 1 to n - i do 
+        begin 
+            if list[j] > list[j+1] then 
+            begin 
+                temp := list[j]; 
+                list[j] := list[j+1]; 
+                list[j+1] := temp; 
+                flag := True; 
+            end; 
+        end; 
+        if not flag then
+            exit;
+    end; 
+end;
+
+var i: 
+    integer; 
+begin 
+    GenerateNumbers(); 
+    writeln('Lista przed BubbleSort:'); 
+    for i := 1 to 50 do 
+        write(list[i], ' '); 
+    writeln; 
+    BubbleSort(); 
+    writeln('Lista po BubbleSort:'); 
+    for i := 1 to 50 do 
+        write(list[i], ' '); 
+    writeln;
 end.
