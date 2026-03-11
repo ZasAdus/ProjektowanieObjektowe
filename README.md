9b4b502212091884f18c74a9256fd37dfcc0dd40
Dawid Ciuba <dawidciuba.com@gmail.com>
2026-03-11 13:32:33 +0100

    4.0

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <h1>Zadania z przedmiotu Projektowanie Obiektowe
 <h2>Zadanie 1:<h2>
 ✅ 3.0 Procedura do generowania 50 losowych liczb od 0 do 100 (https://github.com/ZasAdus/ProjektowanieObjektowe/commit/1d535ad93da1d0b8ec53b559b59442b545094c3d) <br>
-3.5 Procedura do sortowania liczb <br>
+✅ 3.5 Procedura do sortowania liczb (https://github.com/ZasAdus/ProjektowanieObjektowe/commit/2040d35bb2541a3e4e4077676740a057a7224ae0)<br>
 4.0 Dodanie parametrów do procedury losującej określającymi zakres losowania: od, do, ile <br>
 4.5 5 testów jednostkowych testujące procedury <br>
 5.0 Skrypt w bashu do uruchamiania aplikacji w Pascalu via docker <br> <h1>
--- zadanie1/zadanie1.pas
+++ zadanie1/zadanie1.pas
@@ -4,18 +4,31 @@ uses
     SysUtils;
 
 type 
-    IntList = array[1..50] of integer;
+    IntList = array of integer;
 
 var
     list: IntList;
 
-procedure GenerateNumbers();
+procedure GenerateNumbers(min, max, count: integer);
     var
         i: integer;
     begin
+    if max < min then
+    begin
+        write('Błąd: niepoprawny zakres, kończenie programu...');
+        writeln; 
+        Halt;
+    end;
+    if count < 1 then
+    begin
+        write('Błąd: nie można wyenerować listy mniejszej od 1, kończenie programu...');
+        writeln; 
+        Halt;
+    end;
+    SetLength(list, count);
     Randomize;
-    for i := 1 to 50 do
-        list[i] := Random(101);
+    for i := 0 to count - 1 do
+        list[i] := min + Random(max - min + 1);
     end;
 
 procedure BubbleSort(); 
@@ -23,11 +36,11 @@ var
     i, j, n, temp: integer; 
     flag: Boolean;
 begin 
-    n := 50; 
-    for i := 1 to n - 1 do 
+    n := Length(list); 
+    for i := 0 to n - 2 do 
     begin 
         flag := False; 
-        for j := 1 to n - i do 
+        for j := 0 to n - i - 2 do 
         begin 
             if list[j] > list[j+1] then 
             begin 
@@ -42,17 +55,48 @@ begin
     end; 
 end;
 
-var i: 
-    integer; 
-begin 
-    GenerateNumbers(); 
+var 
+    i, code, number, min, max, count: integer; 
+    input: string;
+begin
+    writeln('Podaj minimalną liczbę:'); 
+    readln(input);
+    Val(input, number, code);
+    if code <> 0 then
+    begin
+        write('Błąd: podany input nie jest liczbą całkowitą, kończenie programu...');
+        writeln; 
+        Halt;
+    end;
+    min := number;
+    writeln('Podaj maksymalną liczbę:'); 
+    readln(input);
+    Val(input, number, code);
+    if code <> 0 then
+    begin
+        write('Błąd: podany input nie jest liczbą całkowitą, kończenie programu...');
+        writeln; 
+        Halt;
+    end;
+    max := number;
+    writeln('Podaj ile liczb wygenerować:'); 
+    readln(input);
+    Val(input, number, code);
+    if code <> 0 then
+    begin
+        write('Błąd: podany input nie jest liczbą całkowitą, kończenie programu...');
+        writeln; 
+        Halt;
+    end;
+    count := number;
+    GenerateNumbers(min, max, count); 
     writeln('Lista przed BubbleSort:'); 
-    for i := 1 to 50 do 
+    for i := 0 to Length(list) - 1 do 
         write(list[i], ' '); 
     writeln; 
     BubbleSort(); 
     writeln('Lista po BubbleSort:'); 
-    for i := 1 to 50 do 
+    for i := 0 to Length(list) - 1 do 
         write(list[i], ' '); 
     writeln;
 end.
